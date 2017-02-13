07bb81c5f508ff3664b0c87a4b068da220800c50
Darren Struthers <dstruthers@gmail.com>
2017-02-13 09:53:46 -0800

    Added explicit version number to typefu URL in dependency_links.

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(name='parsing',
       description='Combinatorial parsing framework',
       license='MIT',
       install_requires=['typefu'],
-      dependency_links=['https://github.com/dstruthers/python-typefu/tarball/master#egg=typefu']
+      dependency_links=['https://github.com/dstruthers/python-typefu/tarball/master#egg=typefu-1.0.0']
       )
